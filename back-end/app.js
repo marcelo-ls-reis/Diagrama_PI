85f45f6eff9f0f5ab63d419f46ba57e178ee8dc8
marcelo-ls-reis <100324942+marcelo-ls-reis@users.noreply.github.com>
2022-09-12 20:42:28 -0300

    Aula 12/09/2022

--- back-end/app.js
+++ back-end/app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -22,6 +22,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/* Rotas da aplicação */
+
+const userRoute = require('./routes/user')
+app.use('/user', userRoute)
+
 
 module.exports = app;
